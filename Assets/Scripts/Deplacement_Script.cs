d6ab5f4bf53e137f3111b138e4dff022f576191a
DESKTOP-S0E0I8R\Kévin <kevin.arenas@outlook.fr>
2018-01-10 15:18:58 +0100

    mouvements vaisseau

---
+++ Assets/Scripts/Deplacement_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement_Script : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+        rb.velocity = new Vector2(moveHorizontal * speed, 0);
+    }
+}
