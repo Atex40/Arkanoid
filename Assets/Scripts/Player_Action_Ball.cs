4fd7a8004df8f8d5b686cd21e35f30a7a9b9e94f
Kevin Ibanez <ibanez13keke@gmail.com>
2018-01-10 15:04:38 +0100

    Balle et limite du niveau

---
+++ Assets/Scripts/Player_Action_Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Action_Ball : MonoBehaviour {
+
+    public KeyCode launchKey;
+    public float ejectionSpeed;
+    public float maxSpeed;
+    public Rigidbody2D ballRb;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(launchKey))
+        {
+            ballRb.AddForce(Vector2.up * ejectionSpeed, ForceMode2D.Impulse);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if(ballRb.velocity.y > maxSpeed)
+        {
+            ballRb.velocity = new Vector2(maxSpeed,maxSpeed);
+            //Debug.Log(ballRb.velocity);
+        }
+    }
+}
